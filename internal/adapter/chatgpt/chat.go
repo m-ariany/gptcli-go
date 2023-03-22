0f5eae51b97f29d3cbcd9374432e8a7acc366efc
Amir Hussain Haji Hussaini <hajihussaini@afrabon.com>
2023-03-22 21:56:43 +0330

    Implement newChatCompletionRequest() method of chatgpt.Server

--- internal/adapter/chatgpt/chat.go
+++ internal/adapter/chatgpt/chat.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 
 	"github.com/m-ariany/gptcli/internal/entity"
+
+	"github.com/sashabaranov/go-openai"
 )
 
 func (server *Server) Chat(ctx context.Context, text string) <-chan entity.Message {
@@ -19,4 +21,29 @@ func (server *Server) Chat(ctx context.Context, text string) <-chan entity.Messa
 	}()
 
 	return message
+}
+
+func (server *Server) newChatCompletionRequest(
+	statement string,
+) openai.ChatCompletionRequest {
+	// Ref: https://platform.openai.com/docs/guides/chat/introdution
+	//
+	// Including the conversation history helps the models to give relevantanswers
+	// to the prior conversation.
+	// Because the models have no memory of past requests, all relevant information
+	// must be supplied via the conversation.
+	server.history = append(
+		server.history,
+		openai.ChatCompletionMessage{
+			Role:    "user",
+			Content: statement,
+		},
+	)
+
+	return openai.ChatCompletionRequest{
+		Model:     openai.GPT3Dot5Turbo,
+		MaxTokens: server.cfg.MaxResponseToken,
+		Messages:  server.history,
+		Stream:    true,
+	}
 }
