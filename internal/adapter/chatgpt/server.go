2c4a8c7977acc8d685409b7374e88eb6a22f1d3b
Amir Hussain Haji Hussaini <hajihussaini@afrabon.com>
2023-03-22 21:08:26 +0330

    Implement method of assistant.Assistant.Chat()

---
+++ internal/adapter/chatgpt/server.go
@@ -0,0 +1,32 @@
+package chatgpt
+
+import (
+	"github.com/m-ariany/gptcli/internal/config"
+
+	"github.com/sashabaranov/go-openai"
+)
+
+// Server implements contract.ChatGPTServer interface
+type Server struct {
+	cfg     config.ChatGPTConfig
+	client  *openai.Client
+	history []openai.ChatCompletionMessage
+}
+
+func New(cfg config.ChatGPTConfig) *Server {
+	return &Server{
+		cfg:    cfg,
+		client: openai.NewClient(cfg.APIKey),
+		// Typically, a conversation is formatted with an system message first,
+		// followed by alternating user and assistant messages.
+		// Ref: https://platform.openai.com/docs/guides/chat/introduction
+		//
+		// TODO: the system instructing message must be taken from the user
+		history: []openai.ChatCompletionMessage{
+			{
+				Role:    "system",
+				Content: "You are a helpful AI that answers my questions",
+			},
+		},
+	}
+}
--- internal/interactor/assistant/chat.go
+++ internal/interactor/assistant/chat.go
@@ -2,6 +2,7 @@ package assistant
 
 import (
 	"context"
+	"fmt"
 	"time"
 )
 
@@ -9,14 +10,19 @@ func (assistant *Assistant) Chat(statement string) <-chan string {
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 
-	_ = ctx
-
 	text := make(chan string)
-
 	go func() {
 		defer close(text)
 
-		text <- "I'm an AI language model"
+		for message := range assistant.server.Chat(ctx, statement) {
+			if message.Error != nil {
+				text <- fmt.Sprintf("%s", message.Error)
+
+				return
+			}
+
+			text <- message.Data
+		}
 	}()
 
 	return text
