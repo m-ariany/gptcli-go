d345b74c1dfacb4df175bb5c1b079a9b5f74d3c3
Amir Hussain Haji Hussaini <hajihussaini@afrabon.com>
2023-03-23 13:39:25 +0330

    Refactoring the code

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,19 +2,20 @@ package main
 
 import (
 	"fmt"
-	"runtime/debug"
 
 	"github.com/m-ariany/gptcli/internal/adapter/chatgpt"
 	"github.com/m-ariany/gptcli/internal/config"
 	"github.com/m-ariany/gptcli/internal/delivery/cli"
 	"github.com/m-ariany/gptcli/internal/interactor/assistant"
-
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
-	"github.com/spf13/cobra"
 )
 
 func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
 	cfg := config.NewConfig()
 
 	chatGPT := chatgpt.New(cfg.ChatGPT)
@@ -23,28 +24,4 @@ func main() {
 
 	shell := cli.New(cfg.Shell, assistant)
 	shell.Run()
-}
-
-func run(cmd *cobra.Command, args []string) {
-	out, err := config.LogOutput()
-	if err != nil {
-		panic(err)
-	}
-
-	defer func() {
-		if out != nil {
-			_ = out.Close()
-		}
-	}()
-
-	defer func() {
-		if err := recover(); err != nil {
-			log.Error().Msgf("Oops! %v", err)
-			log.Error().Msg(string(debug.Stack()))
-			fmt.Printf("%v.\n", err)
-		}
-	}()
-
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: out})
-	zerolog.SetGlobalLevel(config.DefaultLogLevel)
 }
