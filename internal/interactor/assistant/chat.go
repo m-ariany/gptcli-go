5ee38e70b6424cb1a4a53859411abd6715c879b3
Amir Hussain Haji Hussaini <hajihussaini@afrabon.com>
2023-03-27 10:34:53 +0330

    Fix bug of context canceled

--- internal/interactor/assistant/chat.go
+++ internal/interactor/assistant/chat.go
@@ -7,11 +7,12 @@ import (
 )
 
 func (assistant *Assistant) Chat(statement string) <-chan string {
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-	defer cancel()
-
 	text := make(chan string)
+
 	go func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+		defer cancel()
+
 		defer close(text)
 
 		for message := range assistant.server.Chat(ctx, statement) {
